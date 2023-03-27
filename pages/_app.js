674303953bfbca4e9ab5b577109a4459aac526d1
Piotr <piotr.bania@europe.com>
2023-03-27 18:58:06 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import React from 'react'
+import { motion as m, AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.scss'
+import '../styles/layout/navbar.scss'
+import '../styles/layout/footer.scss'
+import Layout from '../components/layout/Layout'
+
+
+
+function MyApp({ Component, pageProps, router }) {
+
+    return (
+        <Layout>
+            <Component {...pageProps} key={router.route} />
+        </Layout>
+    )
 }
+
+export default MyApp
